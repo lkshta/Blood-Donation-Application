ca601f129727853940ebcd85fda9b8b6da562f5c
Lakshita <2003lakshita@gmail.com>
2024-11-26 20:12:07 +0530

    added middleware

--- Backend/app.js
+++ Backend/app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const authRoute = require("./routes/auth");
-const donorRoute= require("./routes/donor");
-const prospectRoute= require("./routes/prospect")
-module.exports=app;
+const donorRoute = require("./routes/donor");
+const prospectRoute = require("./routes/prospect");
+module.exports = app;
 
 //CORS
 app.use(cors());
@@ -13,6 +13,6 @@ app.use(cors());
 app.use(express.json());
 
 // ROUTES
-app.use("/api/v1/auth",authRoute);
-// app.use("/api/v1/donors", donorRoute);
-// app.use("/api/v1/prospects", prospectRoute);
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/donors", donorRoute);
+app.use("/api/v1/prospects", prospectRoute);
