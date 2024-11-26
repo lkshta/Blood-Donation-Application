75c2bd67b8beb091cfa48ba7e7e6b95d7ea6fd2a
Lakshita <2003lakshita@gmail.com>
2024-11-27 02:25:44 +0530

    added new files in frontend

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Featured from "../components/Featured";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Featured />
+    </div>
+  );
 };
 
 export default Home;
