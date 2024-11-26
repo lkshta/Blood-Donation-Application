23449c4b6ba78862c3453f5ef59d811a2928b2ba
Lakshita <2003lakshita@gmail.com>
2024-11-26 17:39:01 +0530

    authentication

--- Backend/index.js
+++ Backend/index.js
@@ -6,8 +6,8 @@ dotenv.config();
 //PORT
 const PORT = process.env.PORT;
 
-//SERVER
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
-  dbConnection();
-});
+// SERVER
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
+    dbConnection();
+})
